c17a8ea0e729c19148f048128cb48118ecba574c
Om Tambat <omtambat284@gmail.com>
2023-10-25 03:30:01 +0530

    Player Movement Done
    Jumping Errors fixed and ready for item collection part

--- Assets/Scripts/PlayerControlls.cs
+++ Assets/Scripts/PlayerControlls.cs
@@ -1,3 +1,5 @@
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +7,72 @@ using UnityEngine;
 public class PlayerControlls : MonoBehaviour
 {
     private Rigidbody2D rb;
-    // Start is called before the first frame update
+    private BoxCollider2D collider;
+    private SpriteRenderer spriteRenderer;
+    private Animator anime;
+    [SerializeField] private LayerMask jumpmingGround;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+    private enum MovementState{idle, Running, jumping, falling}
+    private float dirX = 0f;
+    
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anime = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
+   
     private void Update()
     {
-        if(Input.GetKeyDown("space"))
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump") && isGrounded())
         {
-            rb.velocity = new Vector3(0, 12f, 0);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        AnimationUpdate();
+       
+    }
+
+    private void AnimationUpdate()
+    {
+        MovementState state;
+        if(dirX > 0f)
+        {
+            state = MovementState.Running;
+            spriteRenderer.flipX = false;
+        }
+        else if(dirX < 0f)
+        {
+            state = MovementState.Running;
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+           state = MovementState.idle;
+        }
+
+        if(rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        anime.SetInteger("state", (int) state);
+    }
+
+
+    private bool isGrounded()
+    {
+       return Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, .1f, jumpmingGround);
     }
 }
